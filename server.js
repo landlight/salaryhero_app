bc549b3175c3bf4070cc295746b93ad48f88d1ae
Amity <amity@Light-MacBook-Pro.local>
2022-11-01 21:02:46 +0700

    :sparkles: add healthcheck swagger and swagger example

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 
@@ -20,13 +22,21 @@ db.sequelize.sync();
 // Setup server port
 const port = process.env.NODE_DOCKER_PORT || 8080;
 
-const basePath = "/apis";
 // Send message for default URL
+
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument),
+);
+
+
 app.get("/", (req, res) => res.status(200).send("Salary Hero APIs are here"));
 
 // Use Api routes in the App
 const routes = require("./src/app/routes");
-app.use(basePath, routes);
+app.use(routes);
+
 
 // Launch app to listen to specified port
 const server = app.listen(port, function() {
